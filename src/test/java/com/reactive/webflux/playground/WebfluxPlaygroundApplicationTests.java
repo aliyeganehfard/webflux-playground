841489bcc7e6b00106a04e3c217e25ae2442d012
yeganefard <aliyeganefard@gamil.com>
2025-12-05 18:35:35 +0330

    init spring webflux

---
+++ src/test/java/com/reactive/webflux/playground/WebfluxPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.webflux.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
